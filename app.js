8de874d7b6aef997e777c0c8e849cb6ddb2b347f
Hiram Chavez <virtual.liga@gmail.com>
2022-04-08 16:20:12 -0500

    Se instala mongoose y se conecta app con MongoDB

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser'); //Parsea cuerpo de peticiones
 const cors = require('cors'); //Seguridad en peticiones HTTP
 const debug = require('debug')('app:main'); //Debug de la aplicación
 require('dotenv').config(); //Configura variables de entorno
+const mongoose = require('mongoose'); //
 
 //Creamos la aplicación
 const app = express();
@@ -18,6 +19,23 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false})); //Parsea el body
 app.use(bodyParser.json()); //Datos a JSONs
 app.use(cors()); //Seguridad en peticiones
+
+//Obtenemos los datos para la conexión con la BD (de .env)
+const dbUser = process.env.DB_USER; //Usuario de la BD
+const dbPass = process.env.DB_PASS; //Su password
+const dbName = process.env.DB_NAME; //Nombre de la BD
+
+//Creamos la conexión a la BD econ MongoDB usando Mongoose
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.kvrrq.mongodb.net/${dbName}?retryWrites=true&w=majority`)
+    .then(()=>{ //Si todo salió bien
+        debug(`Conexión a la BD ${dbName} exitosa`);
+    })
+    .catch((error) => {//Si algo salió mal
+        debug(`Error en la conexión a la BD ${dbName}`, error);
+    });
+
+//Debuguear mongoose
+mongoose.set('debug', true);
 
 //Middleware para gestionar los endpoints mediante un ruteador
 // purificadora_api/v1 --> Raíz de la API
