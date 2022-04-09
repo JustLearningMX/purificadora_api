43bf7f3aa523a855b20edd768c64844f91bc16a1
Hiram Chavez <virtual.liga@gmail.com>
2022-04-09 11:02:00 -0500

    Se crea el modelo Usuario, se importa al app.js

--- app.js
+++ app.js
@@ -4,13 +4,16 @@
  * al servidor en escucha de peticiones del cliente.
  */
 
-//Se importan las dependecias
+//Se importan las dependencias
 const express = require('express'); //Para levantar un servidor
 const bodyParser = require('body-parser'); //Parsea cuerpo de peticiones
 const cors = require('cors'); //Seguridad en peticiones HTTP
 const debug = require('debug')('app:main'); //Debug de la aplicación
 require('dotenv').config(); //Configura variables de entorno
-const mongoose = require('mongoose'); //
+const mongoose = require('mongoose'); //ODM para MongoDB
+
+//Importamos los Modelos
+require('./models/Usuario');
 
 //Creamos la aplicación
 const app = express();
@@ -25,7 +28,8 @@ const dbUser = process.env.DB_USER; //Usuario de la BD
 const dbPass = process.env.DB_PASS; //Su password
 const dbName = process.env.DB_NAME; //Nombre de la BD
 
-//Creamos la conexión a la BD econ MongoDB usando Mongoose
+//Creamos la conexión a la BD con MongoDB usando Mongoose
+// Candena de conexión obtenida desde MongoDB Atlas
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.kvrrq.mongodb.net/${dbName}?retryWrites=true&w=majority`)
     .then(()=>{ //Si todo salió bien
         debug(`Conexión a la BD ${dbName} exitosa`);
---
+++ models/Usuario.js
@@ -0,0 +1,90 @@
+/**
+ * Modelo Usuario que define un esquema mongoose
+ * y lo relaciona con una colección de MongoDB
+ * y sus campos
+ */
+
+//Importamos mongoose
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator"); //Para validar email y teléfono.
+
+// Esquema para el Usuario: Se incluyen los campos y sus validaciones
+const UsuarioSchema = new mongoose.Schema(
+  {
+    //Campos requeridos para SignUp
+    nombre: { type: String, required: [true, "No puede estar vacío"] },
+    apellidos: { type: String, required: [true, "No puede estar vacío"] },
+    tipo: {
+      type: String,
+      enum: ["admin", "empleado", "cliente"],
+      required: [true, "No puede estar vacío"],
+    },
+    email: {
+      type: String,
+      required: [true, "No puede estar vacío"],
+      unique: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "es inválido"],
+      index: true,
+    },
+    telefono: {
+      type: String,
+      required: [true, "No puede estar vacío"],
+      unique: true,
+      match: [/^[0-9]+$/, "Es inválido"],
+    },
+    password: {
+      type: String,
+      required: [true, "No puede estar vacío"],
+    },
+
+    //Campos opcionales que se pueden Actualizar una vez dado de alta
+    direccion: String,
+    ciudad: String,
+    cp: { 
+        type: String,
+        match: [/^[0-9]+$/, "Es inválido"],
+    },
+    rfc: String,
+    emailRecuperacion: {
+        type: String,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, "es inválido"],
+    },
+    telefonoRecuperacion: {
+        type: String,
+        match: [/^[0-9]+$/, "Es inválido"],
+    },
+  },
+  {
+    timestamps: true, //Fecha de creación y/o modificación
+    collection: "usuarios", //Se define la colección (tabla) para este esquema
+  }
+); 
+
+// Usando plugin de validación para que no se repitan correos ni teléfonos
+UsuarioSchema.plugin(uniqueValidator, { message: "Ya existe" }); 
+
+//Se define la correspondencia entre el Modelo y el esquema creado.
+mongoose.model("Usuario", UsuarioSchema); 
+
+//Función que devuelve datos públicos del esquema creado
+UsuarioSchema.methods.publicData = ()=>{
+    return {
+        id: this.id,
+        nombre: this.nombre,
+        apellidos: this.apellidos,
+        tipo: this.tipo,
+        email: this.email,
+        telefono: this.telefono,
+        direccion: this.direccion,
+        ciudad: this.ciudad,
+        cp: this.cp,
+        rfc: this.rfc,
+        emailRecuperacion: this.emailRecuperacion,
+        telefonoRecuperacion: this.telefonoRecuperacion,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt,
+        password: this.password
+    };
+};
